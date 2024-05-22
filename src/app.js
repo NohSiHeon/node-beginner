9373f3b8e1eea22fac52a720a59696cbaf5e8dc3
NohSiHeon <shtlgjs016@naver.com>
2024-05-22 23:57:15 +0900

    feat: 에러 처리 미들웨어 구현

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from './constants/env.constant.js';
 import { productRouter } from './routers/products.router.js';
 import { connect } from './schemas/index.js';
+import { errorHandler } from './middlewares/error-handler.middleware.js';
 
 connect();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', productRouter);
 
+app.use(errorHandler);
 app.listen(SERVER_PORT, () => {
 	console.log(`Server is listening on ${SERVER_PORT}`);
 });
