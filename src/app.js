5943230915c516c6d10f38d4348aa08c2348d862
NohSiHeon <shtlgjs016@naver.com>
2024-05-22 21:23:52 +0900

    feat: 상품 생성 API 구현

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { SERVER_PORT } from './constants/env.constant.js';
+import { productRouter } from './routers/products.router.js';
+import { connect } from './schemas/index.js';
+
+connect();
 
 const app = express();
 
-app.get('/', (req, res) => {
-	return res.json('hello');
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', productRouter);
+
 app.listen(SERVER_PORT, () => {
 	console.log(`Server is listening on ${SERVER_PORT}`);
 });
